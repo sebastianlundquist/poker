337bf1615d2c20284149908827a8efa034e12f66
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2020-04-12 11:44:50 +0200

    Add friendly string representation extension method to PokerHandRanking

--- src/CardEngine/PokerHandRanking.cs
+++ src/CardEngine/PokerHandRanking.cs
@@ -1,4 +1,6 @@
-﻿namespace CardEngine
+﻿using System.Linq;
+
+namespace CardEngine
 {
     /// <summary>
     /// Represents the ranking of a PokerHand.
@@ -15,5 +17,22 @@
         FullHouse,
         FourOfAKind,
         StraightFlush
+    }
+
+    /// <summary>
+    /// Extension methods for the PokerHandRanking enum.
+    /// </summary>
+    /// <see cref="PokerHandRanking"/>
+    public static class PokerHandRankingExtensions
+    {
+        /// <summary>
+        /// Converts the PokerHandRanking to its equivalent friendly string representation.
+        /// </summary>
+        /// <param name="ranking">The PokerHandRanking.</param>
+        /// <returns>The friendly string representation.</returns>
+        public static string ToFriendlyString(this PokerHandRanking ranking)
+        {
+            return string.Concat(ranking.ToString().Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+        }
     }
 }
